7ded1f315c5d71cc4597fc70a816a6aff02059e0
xilin <xilin@mafengwo.com>
2017-06-28 15:31:27 +0800

    test lightweight

--- data/text/jieba_test.py
+++ data/text/jieba_test.py
@@ -16,8 +16,7 @@ if len(args) < 1:
     print(USAGE)
     sys.exit(1)
 
-# file_name = args[0]
-file_name = '/home/tutumeimei1023/DeepQA2/data/text/content.txt'
+file_name = args[0]
 
 if opt.topK is None:
     topK = 10
@@ -26,11 +25,6 @@ else:
 
 content = open(file_name, 'rb').read()
 
-tags = jieba_test.analyse.extract_tags(content, topK=topK)
-
-f1 = open('/home/tutumeimei1023/DeepQA2/data/text/content_new.txt', 'w')
-for tag in tags:
-    f1.write(tag)
-
+tags = jieba.analyse.extract_tags(content, topK=topK)
 
 print(",".join(tags))
