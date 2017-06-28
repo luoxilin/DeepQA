8ed1ba378d9a0a2b58413d6e5a2eb9c38b5e3a25
xilin <xilin@mafengwo.com>
2017-06-28 15:26:23 +0800

    test lightweight

--- data/text/jieba_test.py
+++ data/text/jieba_test.py
@@ -1,8 +1,8 @@
 import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),os.path.pardir)))
-import jieba_test
-import jieba_test.analyse
+# import os
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),os.path.pardir)))
+import jieba
+import jieba.analyse
 from optparse import OptionParser
 
 USAGE = "usage:    python extract_tags.py [file name] -k [top k]"
